0b47ca05abb0205b28d21097b3219d027e973991
Pavl1uk <pavljuk.jurij.clg@chnu.edu.ua>
2024-10-01 23:37:51 +0300

    1

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,2 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListyIterator<T> : IEnumerable<T>
+{
+    private List<T> items;
+    private int index;
+
+    public ListyIterator(params T[] items)
+    {
+        this.items = new List<T>(items);
+        this.index = 0;
+    }
+
+    public bool Move()
+    {
+        if (HasNext())
+        {
+            index++;
+            return true;
+        }
+        return false;
+    }
+
+    public bool HasNext()
+    {
+        return index < items.Count - 1;
+    }
+
+    public void Print()
+    {
+        if (items.Count == 0)
+        {
+            throw new InvalidOperationException("Invalid Operation!");
+        }
+        Console.WriteLine(items[index]);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            yield return items[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+        ListyIterator<string> listyIterator = null;
+
+        while (input != "END")
+        {
+            string[] commandParts = input.Split();
+            string command = commandParts[0];
+
+            switch (command)
+            {
+                case "Create":
+                    listyIterator = new ListyIterator<string>(commandParts.Length > 1 ? commandParts[1..] : new string[0]);
+                    break;
+                case "Move":
+                    Console.WriteLine(listyIterator.Move());
+                    break;
+                case "HasNext":
+                    Console.WriteLine(listyIterator.HasNext());
+                    break;
+                case "Print":
+                    try
+                    {
+                        listyIterator.Print();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case "PrintAll":
+                    foreach (var item in listyIterator)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                    break;
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
---
+++ iterator3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Person : IComparable<Person>
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public string City { get; private set; }
+
+    public Person(string name, int age, string city)
+    {
+        Name = name;
+        Age = age;
+        City = city;
+    }
+
+    public int CompareTo(Person other)
+    {
+        int result = Name.CompareTo(other.Name);
+        if (result == 0)
+        {
+            result = Age.CompareTo(other.Age);
+            if (result == 0)
+            {
+                result = City.CompareTo(other.City);
+            }
+        }
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Person> people = new List<Person>();
+        string input;
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] data = input.Split();
+            string name = data[0];
+            int age = int.Parse(data[1]);
+            string city = data[2];
+
+            people.Add(new Person(name, age, city));
+        }
+
+        int targetIndex = int.Parse(Console.ReadLine()) - 1;
+        Person targetPerson = people[targetIndex];
+
+        int matches = 0;
+        int nonMatches = 0;
+
+        foreach (var person in people)
+        {
+            if (targetPerson.CompareTo(person) == 0)
+            {
+                matches++;
+            }
+            else
+            {
+                nonMatches++;
+            }
+        }
+
+        if (matches == 1)
+        {
+            Console.WriteLine("No matches");
+        }
+        else
+        {
+            Console.WriteLine($"{matches} {nonMatches} {people.Count}");
+        }
+    }
+}
